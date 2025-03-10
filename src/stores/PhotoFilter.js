bfe4caa3a79a3ab974819d161fc40d51559dce08
Olesya Figurovskaya <lisonkadrug@gmail.com>
2025-03-10 21:23:02 +0300

    filter 0.5

--- src/stores/PhotoFilter.js
+++ src/stores/PhotoFilter.js
@@ -1,30 +1,54 @@
 import { defineStore } from "pinia";
 import { ref } from 'vue';
+import { usePhotoList } from './PhotoList'
 
 export const usePhotoFilter = defineStore('photoFilter', () => {
     const inputValue = ref('');
     const error = ref(' ');
-    const numberAlbum = ref(Number);
+    const numberAlbum = ref([]);
+
+    const photoList = usePhotoList()
+
+
 
     const validateFilter = () => {
-        if(/\d|\d\s/.test(inputValue.value)) {
-            numberAlbum.value = inputValue.value
+        if (/\d|\d\s/.test(inputValue.value)) {
+            for (let elem of inputValue.value.split(' ')) {
+                console.log(elem);
+                numberAlbum.value.push(Number(elem))
+            }
+            console.log(numberAlbum.value);
+            // numberAlbum.value = inputValue.value
             inputValue.value = '';
             error.value = '';
-            console.log(numberAlbum.value);
         } else {
             error.value = 'Введено некорректное значение. Введите номер альбома (или несколько через пробел)';
             inputValue.value = '';
         }
     };
 
-    const goFilter = () => {
-        let z = numberAlbum.value
-        console.log(numberAlbum.value);
-        
+    const goFilter =  () => {
+        // photoList.allList.value = ref([]);
+        // console.log(photoList.allList.value);
+        photoList.loading = true;
+        setTimeout(()  => {
+            photoList.allList.value = ref([]);
+            console.log(photoList.allList.value);
+            photoList.loading = false;
+            const myLink = 'https://jsonplaceholder.typicode.com/photos?'
+
+            let apiUrl = `${myLink}`;
+
+            numberAlbum.value.forEach((x) => {
+                apiUrl += `albumId=${x}&`;
+                console.log(apiUrl);
+                return photoList.allList.value = fetch(apiUrl)
+                .then((response) => response.json())
+                .then((json) => photoList.allList = (json));
+            });
+            return (photoList.allList.value);
+        }, 2000);
     };
-
-
 
     return {
         validateFilter,
@@ -33,5 +57,4 @@ export const usePhotoFilter = defineStore('photoFilter', () => {
         numberAlbum,
         goFilter
     }
-    
 })
